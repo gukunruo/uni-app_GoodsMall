0bedff90ed9aaee7c238ebaeb574d60c3bedb7b8
gukunruo <676968838@qq.com>
2023-08-17 18:33:31 +0800

    feat: 完成我的页面的组合式函数的封装以及设置页面分包预加载的配置

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,41 +1,96 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-// 导入请求拦截器实用工具 和我们vue3中二次封装的axios[request]是相同的
-import { http } from '@/utils/http'
-
+import { useGuessList } from '@/composables/HwGuess'
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+// 订单选项
+const orderTypes = [
+  { type: 1, text: '待付款', icon: 'icon-currency' },
+  { type: 2, text: '待发货', icon: 'icon-gift' },
+  { type: 3, text: '待收货', icon: 'icon-check' },
+  { type: 4, text: '待评价', icon: 'icon-comment' },
+]
+// 获取会员信息
 const memberStore = useMemberStore()
-// 测试拦截器函数 await async 异步回调
-const getData = async () => {
-  // 发送请求，需要传入我们需要的类型去匹配泛型【使用我们封装的request请求api 类似于二次封装的axios】
-  const res = await http<string[]>({
-    method: `GET`,
-    url: `/home/banner`,
-    header: {},
-  })
-}
+
+// 引入组合式函数中的租价实例以及触底刷新函数
+const { guessRef, onScrolltolower } = useGuessList()
 </script>
 
 <template>
-  <view class="my">
-    <view>会员信息：{{ memberStore.profile }}</view>
-    <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '好物商城',
-          token: 'ggbone',
-        })
-      "
-      size="mini"
-      plain
-      type="primary"
-    >
-      保存用户信息
-    </button>
-    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
-    <button @tap="getData" size="mini" type="default">test request</button>
-  </view>
+  <scroll-view enable-back-to-top @scrolltolower="onScrolltolower" class="viewport" scroll-y>
+    <!-- 个人资料 -->
+    <view class="profile" :style="{ paddingTop: safeAreaInsets!.top + 'px' }">
+      <!-- 情况1：已登录 -->
+      <view class="overview" v-if="memberStore.profile">
+        <navigator url="/pagesMember/profile/profile" hover-class="none">
+          <image class="avatar" :src="memberStore.profile.avatar" mode="aspectFill"></image>
+        </navigator>
+        <view class="meta">
+          <!-- nickname昵称 没有就展示账号名 -->
+          <view class="nickname">
+            {{ memberStore.profile.nickname || memberStore.profile.account }}
+          </view>
+          <navigator class="extra" url="/pagesMember/profile/profile" hover-class="none">
+            <text class="update">更新头像昵称</text>
+          </navigator>
+        </view>
+      </view>
+      <!-- 情况2：未登录 -->
+      <view class="overview" v-else>
+        <navigator url="/pages/login/login" hover-class="none">
+          <image
+            class="avatar gray"
+            mode="aspectFill"
+            src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-04-06/db628d42-88a7-46e7-abb8-659448c33081.png"
+          ></image>
+        </navigator>
+        <view class="meta">
+          <navigator url="/pages/login/login" hover-class="none" class="nickname">
+            未登录
+          </navigator>
+          <view class="extra">
+            <text class="tips">点击登录账号</text>
+          </view>
+        </view>
+      </view>
+      <navigator class="settings" url="/pagesMember/setting/setting" hover-class="none">
+        设置
+      </navigator>
+    </view>
+    <!-- 我的订单 -->
+    <view class="orders">
+      <!-- 订单第一层 -->
+      <view class="title">
+        我的订单
+        <navigator class="navigator" url="/pagesOrder/list/list?type=0" hover-class="none">
+          查看全部订单<text class="icon-right"></text>
+        </navigator>
+      </view>
+      <!-- 订单第二层 -->
+      <view class="section">
+        <!-- 四个订单信息 -->
+        <navigator
+          v-for="item in orderTypes"
+          :key="item.type"
+          :class="item.icon"
+          :url="`/pagesOrder/list/list?type=${item.type}`"
+          class="navigator"
+          hover-class="none"
+        >
+          {{ item.text }}
+        </navigator>
+        <!-- 售后 -->
+        <button class="contact icon-handset" open-type="contact">售后</button>
+      </view>
+    </view>
+    <!-- 猜你喜欢 -->
+    <view class="guess">
+      <HwGuess ref="guessRef" />
+    </view>
+  </scroll-view>
 </template>
 
 <style lang="scss">
-//
+@import './styles/my.scss';
 </style>
